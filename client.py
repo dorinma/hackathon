7bd2ca46222bea9ee01705e87c7b2c68524c1598
edentan <48632353+edentan@users.noreply.github.com>
2020-12-31 18:42:50 +0200

    last client

--- client.py
+++ client.py
@@ -3,6 +3,8 @@ import struct
 import time
 import getch
 import threading
+from scapy.all import get_if_addr
+import ipaddress
 
 class bcolors:
     HEADER = '\033[95m'
@@ -15,15 +17,33 @@ class bcolors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
+
+msg_format = '!IBH'
+cookie = 0xfeedbeef
+msg_type = 0x2
+clientPort = 13117
+BUFFERSIZE = 1024
+global game_on 
+game_on = True
+net = input("Please insert the number (2 for testing, any other character for playing)\n")
+temp_host = get_if_addr('eth1') +'/16'
+if net == '2':
+    temp_host = get_if_addr('eth2') +'/16'
+broadcast_host = str(ipaddress.ip_network(temp_host, False).broadcast_address)
+print(bcolors.OKBLUE + bcolors.BOLD + "Client started," + bcolors.ENDC) 
+print(bcolors.OKBLUE + "listening for offer requests..." + bcolors.ENDC)
+
 def press_chars(sock):
     try:
         while True: #get char from the keyboard and send to server
             char = getch.getch() 
-            sock.send(char.encode())
+            if game_on:
+                sock.send(char.encode())
+            else:
+                break
     except:
         pass
-print(bcolors.OKBLUE + bcolors.BOLD + "Client started," + bcolors.ENDC) 
-print(bcolors.OKBLUE + "listening for offer requests..." + bcolors.ENDC)
+
 while True:
     #initialize variables
     serverIP = '0'
@@ -34,18 +54,17 @@ while True:
             #waiting for a broadcast message from servers
             udpClient = socket(AF_INET, SOCK_DGRAM) 
             udpClient.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
-            udpClient.bind(('172.99.255.255', 13117))
-            data, addr = udpClient.recvfrom(1024)
+            udpClient.bind((broadcast_host, clientPort))
+            data, addr = udpClient.recvfrom(BUFFERSIZE)
             (serverIP, serverPort) = addr
-            (prefix, msgType, msgPort) = struct.unpack('!IBH', data) 
-            if prefix != 0xfeedbeef or msgType != 2: #check if the message format isn't valid
+            (prefix, msgType, msgPort) = struct.unpack(msg_format, data) 
+            if prefix != cookie or msgType != msg_type: #check if the message format isn't valid
                 raise Exception
             print("Received offer from " + serverIP + ", attempting to connect...")
             time.sleep(0.1)
             break
         except:
             time.sleep(0.1)
-            #print(bcolors.FAIL + "Error occured" + bcolors.ENDC)
             pass
     #TCP
     end_msg = ""
@@ -53,11 +72,13 @@ while True:
         tcpClient = socket(AF_INET, SOCK_STREAM) 
         tcpClient.connect((serverIP, msgPort)) #create a TCP connection to a server
         tcpClient.send("ISIS\n".encode()) #send the team name
-        start_msg = tcpClient.recv(1024).decode()
+        start_msg = tcpClient.recv(BUFFERSIZE).decode()
         print (start_msg)
+        game_on = True
         game_thread = threading.Thread(target=press_chars, args=(tcpClient,)) #create the thread that runs the games
         game_thread.start() #start the game
-        end_msg = tcpClient.recv(1024).decode() #get end game message from server
+        end_msg = tcpClient.recv(BUFFERSIZE).decode() #get end game message from server
+        game_on = False
         game_thread.join()
         print(end_msg)
     except:
